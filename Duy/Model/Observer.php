55ecba2e68b1f89ef31d6ff995e535801f5b3106
duypvbk94 <duypvbk94@gmail.com>
2016-08-03 11:39:16 +0700

    First commit

---
+++ Duy/Model/Observer.php
@@ -0,0 +1,19 @@
+<?php
+namespace	Training\Duy\Observer;
+use	Magento\Framework\Event\ObserverInterface;
+class	RegisterVisitObserver	implements	ObserverInterface
+{
+    /**	@var	\Psr\Log\LoggerInterface	$logger	*/
+    protected	$logger;
+
+    public	function	__construct(\Psr\Log\LoggerInterface	$logger)
+    {
+        $this->logger	=	$logger;
+    }
+
+    public	function	execute(\Magento\Framework\Event\Observer
+                                  $observer)
+    {
+        $this->logger->debug('Registered');
+    }
+}
